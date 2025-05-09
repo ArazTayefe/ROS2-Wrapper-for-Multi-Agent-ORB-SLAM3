809cfa066f572b635337d61a0eaf4f57cb96ecce
Moe Tayefe <160788874+ArazTayefe@users.noreply.github.com>
2025-05-08 21:39:56 -0400

    Create all_drones_orbslam3.launch.py

---
+++ launch/all_drones_orbslam3.launch.py
@@ -0,0 +1,50 @@
+# all_drones_orbslam3.launch.py
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='orbslam3',
+            executable='mono',
+            name='orbslam3_alpha',
+            namespace='/alpha',
+            parameters=[
+                {'vocab_path': '/home/walle/ros2_ws/src/orbslam3_ros2/vocabulary/ORBvoc.txt'},
+                {'config_path': '/home/walle/ros2_ws/src/orbslam3_ros2/config/monocular/TUM1.yaml'},
+                {'use_viewer': False}
+            ],
+            remappings=[
+                ('camera', '/Alpha/image_raw')
+            ]
+        ),
+        Node(
+            package='orbslam3',
+            executable='mono',
+            name='orbslam3_bravo',
+            namespace='/bravo',
+            parameters=[
+                {'vocab_path': '/home/walle/ros2_ws/src/orbslam3_ros2/vocabulary/ORBvoc.txt'},
+                {'config_path': '/home/walle/ros2_ws/src/orbslam3_ros2/config/monocular/TUM1.yaml'},
+                {'use_viewer': False}
+            ],
+            remappings=[
+                ('camera', '/Bravo/image_raw')
+            ]
+        ),
+        Node(
+            package='orbslam3',
+            executable='mono',
+            name='orbslam3_charlie',
+            namespace='/charlie',
+            parameters=[
+                {'vocab_path': '/home/walle/ros2_ws/src/orbslam3_ros2/vocabulary/ORBvoc.txt'},
+                {'config_path': '/home/walle/ros2_ws/src/orbslam3_ros2/config/monocular/TUM1.yaml'},
+                {'use_viewer': False}
+            ],
+            remappings=[
+                ('camera', '/Charlie/image_raw')
+            ]
+        )
+    ])
