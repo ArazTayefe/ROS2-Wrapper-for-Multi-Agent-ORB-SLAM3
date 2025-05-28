e09001393fda6a176026dd5c4597b8d0132334c9
Moe Tayefe <160788874+ArazTayefe@users.noreply.github.com>
2025-05-28 11:32:12 -0400

    Update monocular-slam-node.hpp

--- src/monocular/monocular-slam-node.hpp
+++ src/monocular/monocular-slam-node.hpp
@@ -3,33 +3,36 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-
+#include "mocap4r2_msgs/msg/rigid_bodies.hpp"
 #include <cv_bridge/cv_bridge.h>
-
 #include "System.h"
-#include "Frame.h"
-#include "Map.h"
-#include "Tracking.h"
 
-#include "utility.hpp"
+#include <fstream>
+#include <unordered_map>
 
 class MonocularSlamNode : public rclcpp::Node
 {
 public:
     MonocularSlamNode(ORB_SLAM3::System* pSLAM);
-
     ~MonocularSlamNode();
 
 private:
     using ImageMsg = sensor_msgs::msg::Image;
+    using RigidBodiesMsg = mocap4r2_msgs::msg::RigidBodies;
 
-    void GrabImage(const sensor_msgs::msg::Image::SharedPtr msg);
+    void GrabImage(const ImageMsg::SharedPtr msg);
+    void MocapCallback(const RigidBodiesMsg::SharedPtr msg);
 
     ORB_SLAM3::System* m_SLAM;
-
     cv_bridge::CvImagePtr m_cvImPtr;
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_image_subscriber;
+    rclcpp::Subscription<ImageMsg>::SharedPtr m_image_subscriber;
+    rclcpp::Subscription<RigidBodiesMsg>::SharedPtr m_mocap_subscriber;
+
+    std::unordered_map<std::string, std::ofstream> mocap_logs_;
+    std::unordered_map<std::string, std::string> id_to_name_;
+
+    double last_frame_time_;
 };
 
 #endif
